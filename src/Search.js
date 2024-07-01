2a52746020c7805420f0d3fa3c4366bfd5555498
lilacilla <149900581+lilacilla@users.noreply.github.com>
2024-07-01 21:05:41 +0100

    Added dictionary api intergration

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    let apiKey = "4c8c7b09546det2d72f3521aa5o882ea";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordEntry(event) {
