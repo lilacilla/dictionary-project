c300dc4ac206469563c1fc584deb0c597d6b2364
lilacilla <149900581+lilacilla@users.noreply.github.com>
2024-07-08 21:22:49 +0100

    Added Photos component & Photo CSS styling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <Search defaultKeyword="happy" />
+          <Search defaultKeyword="scenery" />
         </main>
         <Footer />
       </div>
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Search.css";
 import "./Dictionary.css";
 
@@ -8,21 +9,32 @@ export default function Search(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState("");
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
-  function search() {
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function discover() {
     let apiKey = "4c8c7b09546det2d72f3521aa5o882ea";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "MTCfOAG953c6eoSBtQBqjV5ibJWZF3eLsMlwRpPngpOuB0cgRAcbGXxR";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    search();
+    discover();
   }
 
   function handleKeywordEntry(event) {
@@ -31,7 +43,7 @@ export default function Search(props) {
 
   function load() {
     setLoaded(true);
-    search();
+    discover();
   }
 
   if (loaded) {
@@ -47,6 +59,7 @@ export default function Search(props) {
           <input type="submit" value="Search" className="search-button" />
         </form>
         <Results outcome={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
